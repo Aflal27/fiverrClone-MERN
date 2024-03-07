8032827fdd6b80479a95c71ccda85e0f78873420
Aflal27 <130915738+Aflal27@users.noreply.github.com>
2024-03-07 16:58:33 +0530

    signIn,signUp function created

--- server/index.js
+++ server/index.js
@@ -15,6 +15,18 @@ mongoose
     console.log("DB ERROR", error);
   });
 
+// midelware
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(8000, () => {
   console.log("server is start");
 });
