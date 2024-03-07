486af612a8c1510362b9533aa8ec48844a406403
Aflal27 <130915738+Aflal27@users.noreply.github.com>
2024-03-07 16:41:14 +0530

    userModelCreated

---
+++ server/models/userModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: [admin, user],
+      default: user,
+    },
+    image: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/3607/3607444.png",
+    },
+    country: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    isSeller: {
+      type: Boolean,
+      default: false,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    beingCleared: [
+      {
+        order: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Order",
+        },
+        amount: { type: Number, required: true },
+        clearAt: {
+          type: Date,
+          default: () => Date.now() + 14 * 24 * 60 * 60 * 1000,
+        },
+      },
+    ],
+    level: {
+      type: String,
+      enum: ["New Seller", "Level 1", "Level 2", "Top Rated"],
+      default: "New Seller",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
