cf45dbac6e15a4706fff658dc1c6ee6fe0bf2006
Aflal27 <130915738+Aflal27@users.noreply.github.com>
2024-03-18 06:57:27 +0530

    verify user function

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -2,12 +2,12 @@ import Jwt from "jsonwebtoken";
 import ErrorHandler from "./error.js";
 import User from "../models/userModel.js";
 
-export const verifyToken = async (req, res, next) => {
+export const verifyUser = async (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) return next(new ErrorHandler(401, "Unauthorized"));
 
   const decoded = Jwt.verify(token, process.env.JWT);
-  req.user = await User.findById(decoded.id);
+  req.user = decoded;
   next();
 };
